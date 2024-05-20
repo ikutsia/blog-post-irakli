1e448ee9b69bfecedc7e697020680313476997d2
irakli <ikutsia@gmail.com>
2024-05-20 14:30:20 +0400

    Improved security. Compiled successfully. All folders in correct location

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,14 +5,22 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// Verifying the environment variables (temporarily for debugging)
+console.log("API Key:", process.env.REACT_APP_API_KEY);
+console.log("Auth Domain:", process.env.REACT_APP_AUTH_DOMAIN);
+console.log("Project ID:", process.env.REACT_APP_PROJECT_ID);
+console.log("Storage Bucket:", process.env.REACT_APP_STORAGE_BUCKET);
+console.log("Messaging Sender ID:", process.env.REACT_APP_MESSAGING_SENDER_ID);
+console.log("App ID:", process.env.REACT_APP_APP_ID);
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "blog-post-natia.firebaseapp.com",
-  projectId: "blog-post-natia",
-  storageBucket: "blog-post-natia.appspot.com",
-  messagingSenderId: "613688985396",
-  appId: "1:613688985396:web:d13f99e2e09a7d8f0d4761",
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
