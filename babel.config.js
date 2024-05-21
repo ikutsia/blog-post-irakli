3a38ec824f569397c3aa812736a45d3b57b23753
irakli <ikutsia@gmail.com>
2024-05-21 23:22:23 +0400

    some updates to babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,14 @@
 module.exports = function (api) {
+  // Ensure NODE_ENV is set
+  if (!process.env.NODE_ENV) {
+    throw new Error(
+      'NODE_ENV is not set. Please set it to "development", "test", or "production".'
+    );
+  }
+
   api.cache(true);
 
   const presets = ["react-app"];
-
   const plugins = [
     "@babel/plugin-proposal-private-property-in-object",
     "@babel/plugin-proposal-private-methods",
