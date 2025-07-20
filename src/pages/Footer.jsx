94f756933983315bf1b7f290e72a62b04831236d
irakli <ikutsia@gmail.com>
2025-07-20 16:47:06 +0400

    Update app to previous version

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -6,7 +6,7 @@ import "../styles/Footer.css";
 const Footer = () => {
   return (
     <footer>
-      <p>&copy; 2024 Your Website</p>
+      <p>&copy; 2025. Irakli Kutsia Website. All Rights Reserved.</p>
     </footer>
   );
 };
