143e69fad28b8cd07f1e363c2116114d32a6edbb
irakli <ikutsia@gmail.com>
2025-07-20 15:24:24 +0400

    Test changes to App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,3 @@
-import React, { useState } from "react";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./styles/App.css";
-import "./styles/Home.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import CreatePost from "./pages/CreatePost.jsx";
-import UpdateResume from "./pages/UpdateResume.jsx";
-import Login from "./pages/Login.jsx";
-import Contact from "./pages/Contact";
-import Footer from "./pages/Footer";
-import { signOut } from "firebase/auth";
-import { auth } from "./firebase-config";
-
 function App() {
   return <h1>Hello World</h1>;
 }
